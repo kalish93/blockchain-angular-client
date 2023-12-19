b5051e7d77f19f61203df78a8f1dcb67ada176ba
kalish93 <kaleabtekalign@gmail.com>
2023-12-19 11:18:21 +0300

    feat: scaffold

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class AppComponent {
+  title = 'blockchain-angular-client';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+import { SharedModule } from './shared/shared.module';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    NgxsModule.forRoot([]),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsStoragePluginModule.forRoot(),
+    LoggerModule.forRoot({
+      level: NgxLoggerLevel.DEBUG,
+    }),
+    CoreModule,
+    UserModule,
+    SharedModule,
+    CommonModule, 
+    RouterOutlet
+  ],
+})
+export class AppModule {}
