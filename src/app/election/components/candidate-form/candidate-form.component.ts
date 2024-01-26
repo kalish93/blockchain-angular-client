953d152d1d47633aede310100a6ea444c8867513
fikremariamF <fikremariamfikadud54@gmail.com>
2024-01-26 11:52:01 +0300

    added styling to the html

--- src/app/election/components/candidate-form/candidate-form.component.ts
+++ src/app/election/components/candidate-form/candidate-form.component.ts
@@ -7,11 +7,42 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrl: './candidate-form.component.scss',
 })
 export class CandidateFormComponent {
-  @Input() form = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl(''),
-    image: new FormControl(null),
-  });
+  @Input() form!: FormGroup;
+  isDragOver = false;
 
   constructor() {}
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragOver = true;
+  }
+
+  onDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragOver = false;
+  }
+
+  onFileDrop(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragOver = false;
+    if (event.dataTransfer?.files) {
+      const file = event.dataTransfer.files[0];
+      this.form.patchValue({ image: file });
+      this.form.get('image')?.updateValueAndValidity();
+    }
+  }
+
+  onFileSelect(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files) {
+      const file = input.files[0];
+      this.form.patchValue({ image: file });
+      this.form.get('image')?.updateValueAndValidity();
+    }
+  }
+
+  removeFile(): void {
+    this.form.patchValue({ image: null });
+    this.form.get('image')?.updateValueAndValidity();
+  }
 }
