bbcd907099169e20e69b1b24fac7b7d6b9a1df60
Nahom32 <senaynahom00@gmail.com>
2024-01-27 12:15:59 +0300

    added some smart contract integration

--- src/app/election/state/election.state.ts
+++ src/app/election/state/election.state.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Action, State, StateContext, StateToken } from '@ngxs/store';
 import { CreateElection } from './election.action';
+import { BlockchainService } from '../services/blockchain.service';
 
 export interface ElectionStateModel {
  
@@ -20,11 +21,18 @@ const defaults = {
 })
 @Injectable()
 export class ElectionState {
+  constructor(private blockchainService: BlockchainService){}
   @Action(CreateElection)
   setProgressOff({ setState }: StateContext<ElectionStateModel>,
     {election}: CreateElection) {
         console.log(election);
+        console.log(election.title);
+        //return this.blockchainService.createElection(election.name, election.candidates);
+        let electionData = []
+        for(let i of election.candidates){
+          electionData.push({name:i.name,imgUrl:i.image,Description:i.description})
+        }
     setState({ inprogress: false });
-    return 
+    return this.blockchainService.createElection(election.title, electionData);
   }
 }
