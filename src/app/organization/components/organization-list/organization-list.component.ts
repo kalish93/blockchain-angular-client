6aba250f08128e384992f007b71883f7cdba7062
fikremariamF <fikremariamfikadud54@gmail.com>
2024-02-15 11:24:08 +0300

    UI for organization list corrected

--- src/app/organization/components/organization-list/organization-list.component.ts
+++ src/app/organization/components/organization-list/organization-list.component.ts
@@ -7,6 +7,7 @@ import { CreateOrganizationDialogComponent } from '../create-organization-dialog
 import { SIDE_DIALOG_CONFIG } from '../../../core/constants/dialog-config';
 import { Router } from '@angular/router';
 import { ORGANIZATION_LIST } from '../../../core/constants/routes';
+import { filter, startWith } from 'rxjs';
 
 interface OrganizationListComponentState {
   organizations: Organization[]
@@ -22,13 +23,17 @@ const initOrganizationListComponentState: OrganizationListComponentState = {
   providers: [RxState],
 })
 export class OrganizationListComponent implements OnInit {
-  organizations$ = this.state.select('organizations');
+  organizations$ = this.state.select('organizations').pipe(
+    filter((organizations) => organizations !== null), // Filter out null values
+    startWith([] as Organization[]) // Provide an empty array as the initial value
+  );
   organizations: Organization[] = [];
+  displayedColumns: string[] = ['fullName', 'shortName', 'status'];
   constructor(
     private state: RxState<OrganizationListComponentState>,
     private organizationFacade: OrganizationFacade,
     private dialog: MatDialog,
-    private router: Router,
+    private router: Router
   ) {
     this.state.set(initOrganizationListComponentState);
     this.state.connect('organizations', this.organizationFacade.organizations$);
@@ -40,11 +45,21 @@ export class OrganizationListComponent implements OnInit {
     });
   }
 
+  editOrganization(event: MouseEvent, organization: Organization) {
+    event.stopPropagation(); // Prevent the event from bubbling up to the row click
+    // Handle edit logic here
+  }
+
+  deleteOrganization(event: MouseEvent, organization: Organization) {
+    event.stopPropagation(); // Prevent the event from bubbling up to the row click
+    // Handle delete logic here
+  }
+
   organizationSelected(organization: Organization) {
     this.organizationFacade.dispatchSetSelectedOrganization(organization);
     this.router.navigate([`${ORGANIZATION_LIST}/${organization.id}`]);
   }
-  openCreateOrganizationDialog(){
+  openCreateOrganizationDialog() {
     this.dialog.open(CreateOrganizationDialogComponent, SIDE_DIALOG_CONFIG);
   }
 }
