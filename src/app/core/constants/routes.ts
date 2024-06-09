86f375bfaca94151313f8cb461594ef7ccb3cbf4
suarfel <surafelgetahun01@gmail.com>
2024-06-09 22:29:33 +0300

    Add the routes and end-points url

--- src/app/core/constants/routes.ts
+++ src/app/core/constants/routes.ts
@@ -7,3 +7,5 @@ export const ELECTION_LIST = 'election-list'
 export const ORGANIZATION_LIST = 'organizations'
 export const DASHBOARD_ROUTE = 'dashboard';
 export const USERS_ROUTE = 'users';
+export const FORGET_PASSWORD_ROUTE = 'forget-password';
+
